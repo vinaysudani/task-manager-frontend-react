de688a9a2b955a3283e94df78b80d966ccb06624
Vinay Sudani <vinaysudani9@gmail.com>
2021-07-26 10:03:39 +0530

    Add bootstrap, React-bootstrap, bootswatch, sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './assets/style.scss'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
