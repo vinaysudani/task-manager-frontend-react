5c4853d54a9097024cee8d3a589a4c900d890968
Vinay Sudani <vinaysudani9@gmail.com>
2021-08-02 15:07:05 +0530

    Add profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,23 @@
-const Profile = () => {
-    return <h1>Profile Page</h1>;
+import { Row, Col } from "react-bootstrap";
+
+import ChangePassword from "../components/user/ChangePassword";
+import Profile from "../components/user/Profile";
+import DeleteAccount from "../components/user/DeleteAccount";
+
+const ProfilePage = () => {
+    return (
+        <Row className="mt-4">
+            <Col md={6} className="my-1">
+                <Profile />
+            </Col>
+            <Col md={6} className="my-1">
+                <ChangePassword />
+            </Col>
+            <Col md={12} className="my-1">
+                <DeleteAccount />
+            </Col>
+        </Row>
+    );
 };
 
-export default Profile;
+export default ProfilePage;
